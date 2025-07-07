b0c85d3785c454efecc9be8eb5d13fd0ecee793a
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-07 16:03:42 +0300

    feat(settings): added verify email btn to security settings page site

--- components/settings/SecuritySettings.tsx
+++ components/settings/SecuritySettings.tsx
@@ -48,12 +48,44 @@ const SecuritySettings: React.FC<ProfileSettingsProps> = ({ userData }) => {
         }
     };
 
+    const handleVerifyMail = async () => {
+        try {
+            await axios.post('http://localhost:3000/api/auth/verify-email', {
+                email: userData?.email
+            },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+            toast.success('E-posta doğrulama bağlantısı gönderildi!');
+        } catch (error) {
+            console.error('E-posta doğrulama hatası:', error);
+            toast.error('Bir hata oluştu, lütfen tekrar deneyin.');
+        }
+    };
+
     return (
         <div className="space-y-6">
             {/* Şifre Değiştirme */}
             <div className="bg-dark-800 border border-neutral-600 p-6">
                 <h2 className="text-xl text-white font-pixelify mb-4">Şifre Değiştir</h2>
                 <Button variant='filled' onClick={handleChangePassword}>Şifre Sıfırlama Bağlantısı Al</Button>
+            </div>
+
+
+            {/* Email Doğrula */}
+            <div className="bg-dark-800 border border-neutral-600 p-6">
+                {
+                    userData?.isVerified ? (
+                        <h2 className="text-xl text-green-500 font-pixelify mb- 4">E-posta Adresiniz Doğrulandı</h2>
+                    ) : ( <>
+                            <h2 className="text-xl text-white font-pixelify mb-4">E-Postanı Doğrula</h2>
+                            <Button variant='filled' onClick={handleVerifyMail}>E-Posta Doğrulama Bağlantısı</Button>
+                    
+                    </> )
+                }
             </div>
         </div>
     );
